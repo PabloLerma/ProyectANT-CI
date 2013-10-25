b3e0e3b0836f09398fbd661015e92bb8799a4bcd
PabloLerma <plermam@gmail.com>
2013-10-25 20:22:28 +0200

    Cambio en suma

--- src/ant/Suma.java
+++ src/ant/Suma.java
@@ -1,10 +1,14 @@
 package ant;
 public class Suma {
+	private double resultado = 0;
+	
 	public double getSuma(double operando1, double operando2) {
-		return operando1 + operando2;
+		resultado = operando1 + operando2;
+		return resultado;
 	}
 
 	public double incrementa(double operando) {
-		return operando + 1;
+		resultado = operando + 1;
+		return resultado;
 	}
 }
